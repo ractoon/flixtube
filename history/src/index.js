361473dff332293854a3a1656c79104161ca89a4
ractoon <ractoon@gmail.com>
2021-09-20 21:10:11 -0600

    History microservice

---
+++ history/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+function setupHandlers(app) {
+
+}
+
+function startHttpServer() {
+  return new Promise(resolve => {
+    const app = express();
+    setupHandlers();
+
+    const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+    app.listen(port, () => {
+      resolve();
+    });
+  });
+}
+
+function main() {
+  console.log('Hello World!');
+  return startHttpServer();
+}
+
+main()
+  .then(() => console.log('Microservice online.'))
+  .catch(err => {
+    console.error('Microservice failed to start.');
+    console.error(err && err.stack || err);
+  });
